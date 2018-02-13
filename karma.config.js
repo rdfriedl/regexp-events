86544d159ebda54ab1c88523f6db813ddb688e70
rdfriedl <rdfriedl@gmail.com>
2018-02-13 08:11:26 -0600

    chore: update karma to use headless browsers
    replaced karma-sourcemap-loader with karma-source-map-support
    removed karma-detect-browsers

---
+++ karma.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const rollupConfig = require("./config/rollup.test.config");
+
+module.exports = function(config) {
+	config.set({
+		frameworks: ["source-map-support", "mocha", "sinon-chai", "phantomjs-shim"],
+
+		plugins: [
+			"karma-chrome-launcher",
+			"karma-firefox-launcher",
+			"karma-phantomjs-launcher",
+			"karma-phantomjs-shim",
+
+			"karma-mocha",
+			"karma-mocha-reporter",
+			"karma-sinon-chai",
+			"karma-source-map-support",
+			"karma-rollup-plugin",
+			"karma-coverage",
+			"karma-coverage-html-index-reporter",
+		],
+
+		browsers: ["ChromeHeadless", "FirefoxHeadless", "PhantomJS"],
+
+		customLaunchers: {
+			FirefoxHeadless: {
+				base: "Firefox",
+				flags: ["-headless"],
+			},
+		},
+
+		files: ["test/index.js"],
+
+		preprocessors: {
+			"test/index.js": ["rollup"],
+		},
+
+		rollupPreprocessor: rollupConfig,
+
+		singleRun: true,
+
+		reporters: ["mocha", "coverage", "coverage-html-index"],
+
+		client: {
+			captureConsole: false,
+			mocha: {
+				reporter: "html",
+			},
+		},
+
+		mochaReporter: {
+			output: "autowatch",
+			showDiff: true,
+		},
+
+		coverageReporter: {
+			type: "html",
+			dir: path.resolve(__dirname, "coverage/"),
+		},
+	});
+};
