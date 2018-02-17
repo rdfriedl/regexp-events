313e7112c45f3c446aff6d5d00b7d04e380abedc
rdfriedl <rdfriedl@gmail.com>
2018-02-16 18:54:50 -0600

    test: clean up tests

--- test/scenarios/cjs-bundle.js
+++
@@ -1,8 +0,0 @@
-import EmitterTestSuite from "../Emitter.spec";
-import EventTestSuite from "../Event.spec";
-import { Emitter, Event } from "../../dist/regexp-events";
-
-describe("CommonJS Bundle:", () => {
-	EmitterTestSuite(Emitter, Event);
-	EventTestSuite(Event);
-});
--- test/scenarios/es-bundle.js
+++
@@ -1,8 +0,0 @@
-import EmitterTestSuite from "../Emitter.spec";
-import EventTestSuite from "../Event.spec";
-import { Emitter, Event } from "../../dist/regexp-events.es";
-
-describe("ES Bundle:", () => {
-	EmitterTestSuite(Emitter, Event);
-	EventTestSuite(Event);
-});
